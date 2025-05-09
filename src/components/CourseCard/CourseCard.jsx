9888b72479c96c439a73766595e626c2474b8fba
antnose <antnose28@gmail.com>
2025-05-09 22:29:22 +0600

    In home page show data dynamically without image

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,7 +1,8 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-const CourseCard = ({ img, title, description }) => {
+const CourseCard = ({ course }) => {
+  const { category, course_title, course_description, course_instructor } =
+    course;
   return (
     <div>
       {/* ---------------- card ------------- */}
@@ -9,18 +10,20 @@ const CourseCard = ({ img, title, description }) => {
         <figure className="overflow-hidden rounded-xl">
           <div className="w-full md:h-56">
             <img
-              src={img}
               className="group-hover:scale-105 w-full h-full object-cover transition duration-500 ease-in-out"
               alt="Course Image"
             />
           </div>
         </figure>
         <div className="pt-8 space-y-3 ">
-          <h2 className="card-title">{title}</h2>
-          <p>{description}</p>
-          <div className="divider"></div>
+          <h2 className="card-title">{course_title}</h2>
+          <p>{course_description}</p>
+          <div className="divider">Instructor: {course_instructor}</div>
           <div className="card-actions flex justify-between items-center">
-            <Link className="btn w-full btn-neutral bg-[#07a698] hover:bg-[#01998c] border-none rounded-3xl px-6 py-2 text-white">
+            <Link
+              to={`/${category}`}
+              className="btn w-full btn-neutral bg-[#07a698] hover:bg-[#01998c] border-none rounded-3xl px-6 py-2 text-white"
+            >
               See Details
             </Link>
           </div>
--- src/components/FeaturedCourses/FeaturedCourses.jsx
+++ src/components/FeaturedCourses/FeaturedCourses.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CourseCard from "../CourseCard/CourseCard";
-
-import electrical from "../../assets/Electrical.jpg";
-import mechanical from "../../assets/Mechanical.jpg";
-import civil from "../../assets/Civil.jpg";
-import Computer from "../../assets/Computer.jpg";
-import textile from "../../assets/Textile.jpg";
+import axios from "axios";
 
 const FeaturedCourses = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios(`${import.meta.env.VITE_API_URL}/course`);
+      setCourses(data);
+    };
+    getData();
+  }, []);
+  console.log(courses);
   return (
     <div className="py-30">
       <div>
@@ -19,50 +23,9 @@ const FeaturedCourses = () => {
         </p>
       </div>
       <div className="mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 xl:gap-20">
-        {/* Electrical */}
-        <CourseCard
-          img={electrical}
-          title={"Electrical Engineering"}
-          description={
-            "Electrical engineering is a broad field that involves the study, design, and application of electricity, electronics, and electromagnetism."
-          }
-        />
-
-        {/* Mechanical */}
-        <CourseCard
-          img={mechanical}
-          title={"Mechanical Engineering"}
-          description={
-            "Mechanical engineering is a broad field that deals with the design, analysis, manufacturing, and maintenance of mechanical systems, machines, and devices."
-          }
-        />
-
-        {/* Civil */}
-        <CourseCard
-          img={civil}
-          title={"Civil Engineering"}
-          description={
-            "Civil engineering is a discipline focused on the design, construction, and maintenance of infrastructure like roads, bridges, buildings, and water systems."
-          }
-        />
-
-        {/* Computer */}
-        <CourseCard
-          img={Computer}
-          title={"Computer Science & Engineering"}
-          description={
-            "Computer Science and Engineering (CSE) is an interdisciplinary field that combines aspects of computer science, computer engineering, and software engineering."
-          }
-        />
-
-        {/* Textile */}
-        <CourseCard
-          img={textile}
-          title={"Textile Engineering"}
-          description={
-            "Textile engineering is a field that applies engineering principles to the design, production, and processing of textiles, from raw materials to finished products."
-          }
-        />
+        {courses.map((course, idx) => (
+          <CourseCard key={idx} course={course} />
+        ))}
       </div>
     </div>
   );
