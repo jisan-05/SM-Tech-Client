332ed4cee69a079ade1881fb73efe0ab46a56d8f
antnose <antnose28@gmail.com>
2025-05-10 10:15:41 +0600

    Add addCourse section

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,25 +4,30 @@ import Home from "../pages/Home/Home";
 import ErrorPage from "../pages/ErrorPage";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
+import AddCourse from "../components/AddCourse/AddCourse";
 
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
         path: "/",
-        element: <MainLayout></MainLayout>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children:[
-            {
-                path:"/",
-                element:<Home></Home>
-            },
-            {
-                path:'/login',
-                element:<Login></Login>
-            },
-            {
-                path:'/signup',
-                element:<SignUp></SignUp>
-            }
-        ]
-    },
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+      {
+        path: "/addCourse",
+        element: <AddCourse />,
+      },
+    ],
+  },
 ]);
