f81cd9ae882cbeade95d26f34ce097fb0edcfe20
antnose <antnose28@gmail.com>
2025-05-11 00:28:55 +0600

    add MyPostedCourse page

---
+++ src/pages/MyPostedCourse/MyPostedCourse.jsx
@@ -0,0 +1,10 @@
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { Link } from "react-router-dom";
+
+const MyPostedCourse = () => {
+  return <div></div>;
+};
+
+export default MyPostedCourse;
